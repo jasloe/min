37ce3c515e8a292453ba8bbe3077c329c0e1a342
Jason Loeffler <j@minorscience.com>
2020-02-10 10:47:44 -0500

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 import barba from "../../node_modules/@barba/core"
 import barbaCss from "../../node_modules/@barba/css"
-import MicroModal from "../../mode_modules/micromodal"
 
-
-barba.use(barbaCss);
-
-MicroModal.init();
+// barba.use(barbaCss);
 
 // ES* test
 if (true) {
@@ -13,19 +9,19 @@ if (true) {
   console.log(meowBus); //40
 }
 
-barba.init({
-  transitions: [{
-    name: 'dummy-transition',
-    from: 'home',
+// barba.init({
+//   transitions: [{
+//     name: 'dummy-transition',
+//     from: 'home',
 
-    to: {
-      namespace: [
-        'exhibitions'
-      ]
-    },
-    sync: true,
-    enter() {
+//     to: {
+//       namespace: [
+//         'exhibitions'
+//       ]
+//     },
+//     sync: true,
+//     enter() {
 
-    }
-  }]
-});
+//     }
+//   }]
+// });
