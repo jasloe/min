488b6d3c94504c62b9f6b54bbab247b39d4db215
Jason Loeffler <j@minorscience.com>
2020-02-24 12:28:48 -0500

    decoupled

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,31 @@
-import barba from "../../node_modules/@barba/core"
-import barbaCss from "../../node_modules/@barba/css"
-import MicroModal from "../../mode_modules/micromodal"
+// import barba from "../../node_modules/@barba/core"
+// import barbaCss from "../../node_modules/@barba/css"
+// import MicroModal from "../../mode_modules/micromodal"
 
 
-barba.use(barbaCss);
+// barba.use(barbaCss);
 
-MicroModal.init();
+// MicroModal.init();
 
-// ES* test
-if (true) {
-  let meowBus = 40;
-  console.log(meowBus); //40
-}
+// // ES* test
+// if (true) {
+//   let meowBus = 40;
+//   console.log(meowBus); //40
+// }
 
-barba.init({
-  transitions: [{
-    name: 'dummy-transition',
-    from: 'home',
+// barba.init({
+//   transitions: [{
+//     name: 'dummy-transition',
+//     from: 'home',
 
-    to: {
-      namespace: [
-        'exhibitions'
-      ]
-    },
-    sync: true,
-    enter() {
+//     to: {
+//       namespace: [
+//         'exhibitions'
+//       ]
+//     },
+//     sync: true,
+//     enter() {
 
-    }
-  }]
-});
+//     }
+//   }]
+// });
